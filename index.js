a577632c331614cf38f130eb268d32a75e5466fd
Łukasz Wójcik <lwojcik@lukem.net>
2018-11-13 21:07:32 +0100

    profile endpoints

--- index.js
+++ index.js
@@ -6,13 +6,32 @@ const app = express();
 
 const port = 8883;
 
+const StarCraft2Api = require('../starcraft2api');
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// routes
+app.set('json spaces', 2);
+
+/** Applying JSON indentation for development environment */
+if (process.env.NODE_ENV !== 'production') {
+app.set('json spaces', 2);
+}
+
+// Routes
 
 app.get('/', (req, res) => {
-    res.json('hello world!')
+    res.json({ hello: 'world!' });
+});
+
+// Profile API
+
+app.get('/server/:server/api/profile/static/profile/:region', (req, res) => {
+    const { server, region } = req.params; 
+    const SC2API = new StarCraft2Api(clientID, clientSecret);
+    const static = SC2API.Profile().getStatic(server, region);
+
+    res.json(static);
 });
 
 // starting server
