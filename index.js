496667de76414c78fd34673f74ecfc6a1bc9a742
Łukasz Wójcik <lwojcik@lukem.net>
2018-11-15 20:49:32 +0100

    changes

--- index.js
+++ index.js
@@ -6,38 +6,38 @@ const app = express();
 
 const port = 8883;
 
-const StarCraft2Api = require('../starcraft2api');
+// const BattleNetApi = require('../blizzard-battlenet-api');
 
-const clientID = '123';
-const clientSecret = '456'; 
+// const clientID = '123';
+// const clientSecret = '456';
 
-const SC2API = new StarCraft2Api(clientID, clientSecret);
+// const BnetApi = new BattleNetApi(clientID, clientSecret);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 /** Applying JSON indentation for development environment */
 if (process.env.NODE_ENV !== 'production') {
-    app.set('json spaces', 2);
+  app.set('json spaces', 2);
 }
 
 // Routes
 
 app.get('/', (req, res) => {
-    res.json({ hello: 'world!' });
+  res.json({ hello: 'world!' });
 });
 
 // Profile API
 
-app.get('/server/:server/api/profile/static/profile/:region', (req, res) => {
-    const { server, region } = req.params; 
-    const static = SC2API.Profile().getStatic(server, region);
+// app.get('/server/:server/api/profile/static/profile/:region', (req, res) => {
+//     const { server, region } = req.params;
+//     const static = SC2API.Profile().getStatic(server, region);
 
-    res.json(static);
-});
+//     res.json(static);
+// });
 
 // starting server
 
 module.exports = http.createServer(app).listen(port, () => {
-    console.log(`API server started on port ${port}`); // eslint-disable-line no-console
+  console.log(`API server started on port ${port}`); // eslint-disable-line no-console
 });
