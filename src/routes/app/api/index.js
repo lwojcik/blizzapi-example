ae1e937799edebec90432b3cd34ebcf6384c8800
Łukasz Wójcik <lwojcik@lukem.net>
2019-01-10 22:28:27 +0100

    add accessToken as optional search param

--- src/routes/app/api/index.js
+++ src/routes/app/api/index.js
@@ -8,11 +8,11 @@ const { BattleNetApi } = require('../../../../../blizzard-battlenet-api');
 router.get('/query/:region', async (req, res) => {
   try {
     const { region } = req.params;
-    const { endpoint } = req.query;
+    const { endpoint, accessToken } = req.query;
     const clientId = env.API_BATTLENET_KEY;
     const clientSecret = env.API_BATTLENET_SECRET;
     const BnetApi = new BattleNetApi(region, clientId, clientSecret);
-    const response = await BnetApi.query(endpoint);
+    const response = await BnetApi.query(endpoint, accessToken);
     res.json(response);
   } catch (error) {
     res.send(error);
