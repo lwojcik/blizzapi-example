4141792734eec2bb8eaf5affb76d1b5fff1f92ab
Łukasz Wójcik <lwojcik@lukem.net>
2019-01-10 21:07:08 +0100

    configure dotenv + showcase .query method

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
--- src/routes/app/api/index.js
+++ src/routes/app/api/index.js
@@ -1,15 +1,18 @@
 const router = require('express').Router();
+
+const { env } = process;
 const { BattleNetApi } = require('../../../../../blizzard-battlenet-api');
 
 /** Regions */
 
-router.get('/connect', (req, res) => {
-  const clientId = '123';
-  const clientSecret = '456';
-  const region = 'us';
+router.get('/connect', async (req, res) => {
+  const clientId = env.API_BATTLENET_KEY;
+  const clientSecret = env.API_BATTLENET_SECRET;
+  const region = 'eu';
   const BnetApi = new BattleNetApi(region, clientId, clientSecret);
-  BnetApi.connect();
-  res.json(BnetApi);
+  await BnetApi.connect();
+  const response = await BnetApi.query('/sc2/profile/2/1/5593296');
+  res.json(response);
 });
 
 module.exports = router;
