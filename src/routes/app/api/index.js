ae64d28808e4b9c2681d5f36823f05073d50571a
Łukasz Wójcik <lwojcik@lukem.net>
2019-02-03 12:27:31 +0100

    querySearch examples

--- src/routes/app/api/index.js
+++ src/routes/app/api/index.js
@@ -19,4 +19,35 @@ router.get('/query/:region', async (req, res) => {
   }
 });
 
+router.get('/querySearch/:region', async (req, res) => {
+  try {
+    const { region } = req.params;
+    const { endpoint, accessToken, selector } = req.query;
+    const clientId = env.API_BATTLENET_KEY;
+    const clientSecret = env.API_BATTLENET_SECRET;
+    const BnetApi = new BattleNetApi(region, clientId, clientSecret, accessToken);
+    const response = await BnetApi.querySearch(endpoint, selector);
+    res.json(response);
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+router.get('/queryBatch', async (req, res) => {
+  try {
+    const region = 'us';
+    const endpoints = [
+      '/sc2/profile/1/1/1465407',
+      '/sc2/profile/1/2/242838',
+    ];
+    const clientId = env.API_BATTLENET_KEY;
+    const clientSecret = env.API_BATTLENET_SECRET;
+    const BnetApi = new BattleNetApi(region, clientId, clientSecret);
+    const response = await BnetApi.queryBatch(endpoints);
+    res.json(response);
+  } catch (error) {
+    res.send(error);
+  }
+});
+
 module.exports = router;
