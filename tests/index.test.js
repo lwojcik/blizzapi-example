d9452ee0e0a0f482a234761b47b1321aa43da79a
Łukasz Wójcik <lwojcik@lukem.net>
2019-07-16 20:25:41 +0200

    tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,9 +1,14 @@
 const request = require('supertest');
 const app = require('../src/app');
 
-describe('Test the root path', () => {
-    test('It should response the GET method', async () => {
-        const response = await request(app).get('/');
-        expect(response.statusCode).toBe(200);
-    });
+describe('/', () => {
+  it('should return 200', async () => {
+    const response = await request(app).get('/');
+    expect(response.statusCode).toBe(200);
+  });
+
+  it('should match snapshot', async () => {
+    const response = await request(app).get('/');
+    expect(response.body).toMatchSnapshot();
+  });
 })
